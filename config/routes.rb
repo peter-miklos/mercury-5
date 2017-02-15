48f010503704891e6601a4db73b1d3ba0cbf9e28
Peter Miklos <pt.miklos@gmail.com>
2017-02-15 22:45:06 +0000

    jwt related details added, routes, api_controller, application_controller updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post 'auth_user' => 'authentication#authenticate_user'
+
   scope '/api' do
     scope '/v1' do
       scope '/products' do
