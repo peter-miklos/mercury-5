403175b6bb55e34dae0d3f466549ef5657d4a1d0
Peter Miklos <pt.miklos@gmail.com>
2017-05-09 17:57:38 +0100

    update cors config in api, generate/add login.component, update auth service, app-routing and app.module accordingly

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { Routes, RouterModule }     from '@angular/router';
 
 import { ProductsComponent }        from './products/products.component';
+import { LoginComponent }           from './login/login.component';
 import { AuthGuard }                from './_guards/auth.guard';
 
 const appRoutes: Routes = [
-  // { path: 'login', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductsComponent, canActivate: [AuthGuard] },
   // { path: 'products/new', component: ProductFormComponent, canActivate: [AuthGuard] },
   // { path: 'products/:id', component: ProductDetailComponent, canActivate: [AuthGuard] },
---
+++ web/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }            from '@angular/router';
+
+import { AuthenticationService }  from '../_services/authentication.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  private model: any = {};
+  private loading: boolean = false;
+  private error: string = '';
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
+
+  ngOnInit() {
+    this.authenticationService.logout();
+  }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+        .subscribe(result => {
+          if(result) {
+            this.router.navigate(['/']);
+          } else {
+            this.error = 'Username or password is incorrect!';
+            this.loading = false;
+          }
+        })
+  }
+
+}
