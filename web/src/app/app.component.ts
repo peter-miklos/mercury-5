37cc10d7d501deb3a60b64c866105b426901ba36
Peter Miklos <pt.miklos@gmail.com>
2017-05-09 14:28:52 +0100

    remove app.component.html and css files, update app.component.ts

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <nav>
+    <a routerLink="/">Home</a> |
+    <a routerLink="/products">All products</a> |
+    <a routerLink="/products/new">Add new product</a>
+  </nav>
+  <router-outlet></router-outlet>
+  `
 })
-export class AppComponent {
-  title = 'app works!';
-}
+export class AppComponent {}
