02b240966ae09497900cec8843fc06a94fc4673e
Peter Miklos <pt.miklos@gmail.com>
2017-05-12 23:53:44 +0100

    update navbar in web, refactor auth controller in api

--- api/app/controllers/authentication_controller.rb
+++ api/app/controllers/authentication_controller.rb
@@ -6,33 +6,21 @@ class AuthenticationController < ApplicationController
     if user_info["email"].blank?
       Omniauth::Facebook.deauthorize(params[:fbToken])
     else
-      p user_info
-      p user_info["email"]
       user = User.find_for_database_authentication(email: user_info["email"])
-      p "User:"
-      p user
       if user
         fb_provider = user.user_providers.where(:"provider_uid" => user_info["id"], :"provider" => "facebook")
-        p "FB provider:"
-        p fb_provider
         unless fb_provider
           user.user_providers.create!(provider: "facebook", provider_uid: user_info["id"])
-          p "User after user_provider added:"
-          p user
         end
       else
         user = User.create!(email: user_info["email"], password: "password")
-        p "new User:"
-        p user
         if user && user.valid?
           user.user_providers.create!(provider: "facebook", provider_uid: user_info["id"])
-          p "User after provider added and saved:"
-          p user
         else
           render json: {errors: ['User cannot be created'], status: 500 }, status: :internal_server_error
         end
       end
-      render json: payload(user)
+      render json: payload(user, "facebook")
     end
   end
 
@@ -54,13 +42,14 @@ class AuthenticationController < ApplicationController
 
   private
 
-  def payload(user)
+  def payload(user, user_provider)
     return nil unless user && user.id
     expiration = Time.now.to_i + (60 * 120)
     {
       token: JsonWebToken.encode({user_id: user.id}, expiration),
       user: {id: user.id, email: user.email},
-      expiration: expiration
+      expiration: expiration,
+      user_provider: user_provider
     }
   end
 
